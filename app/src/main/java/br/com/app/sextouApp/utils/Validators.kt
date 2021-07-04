09d05d3909cddbdc885f2b218bad3ff5b98d9085
Rafael <rafafd@hotmail.com>
2021-07-04 19:26:19 -0300

    Layout tela login

--- app/src/main/java/br/com/app/sextouApp/utils/Validators.kt
+++ app/src/main/java/br/com/app/sextouApp/utils/Validators.kt
@@ -1,38 +1,47 @@
 package br.com.app.sextouApp.utils
 
-import android.util.Log
+import android.content.Context
 import android.util.Patterns
+import br.com.app.sextouApp.R
 
 object Validators {
-    fun emailValidator(email:String?): String? {
-        return if(email == null) "Insira um e-mail." else if (!Patterns.EMAIL_ADDRESS.matcher(email).find()) "Insira um e-mail válido." else null
+    fun emailValidator(email:String?, context: Context): String? {
+        return when{
+            email == null -> context.getString(R.string.validation_email_null)
+            !Patterns.EMAIL_ADDRESS.matcher(email).find() -> context.getString(R.string.validation_email_invalid)
+            else -> null
+        }
     }
 
-    fun passwordValidator(password: String?): String? {
-        return if(password == null) "Insira uma senha." else if (password.length < 6) "A senha deve ter no mínimo 6 caracteres." else null
+    fun passwordValidator(password: String?, context: Context): String? {
+        return when {
+            password == null -> context.getString(R.string.validation_password_null)
+            password.length < 6 -> context.getString(R.string.validation_password_minsize)
+            else -> null
+        }
     }
 
-    fun nameValidator(name: String?): String? {
-        return if(name == null) "Insira um nome." else if (name.length < 3) "O nome deve ter ao menos três caracteres." else null
+    fun nameValidator(name: String?, context: Context): String? {
+        return when {
+            name == null -> context.getString(R.string.validation_name_null)
+            name.length < 3 -> context.getString(R.string.validation_name_minsize)
+            else -> null
+        }
     }
 
-    fun cpfValidator(cpf:String?): String? {
-        if (cpf == null) return "Insira um Cpf."
-        if (cpf.length != 11) return "O Cpf deve ter 11 dígitos."
-        if (!Regex("\\d+").matches(cpf)) return "O Cpf deve conter apenas números."
-        if (cpf.all { it == cpf[0] }) return "O Cpf não pode conter todos os dígitos iguais."
-
-        val dv1 = ((0..8).sumBy { (10-it) * cpf[it].toString().toInt() }).times(10).rem(11).let {
+    fun cpfValidator(cpf:String?, context: Context): String? {
+        if (cpf == null) return context.getString(R.string.validation_cpf_null)
+        if (cpf.length != 11) return context.getString(R.string.validation_cpf_minsize)
+        if (!Regex("\\d+").matches(cpf)) return context.getString(R.string.validation_cpf_num)
+        if (cpf.all { it == cpf[0] }) return context.getString(R.string.validation_cpf_equaldigits)
+        val dv1 = ((0..8).sumOf { (10-it) * cpf[it].toString().toInt() }).times(10).rem(11).let {
             if (it >= 10) 0 else it
         }
-
-        val dv2 = ((0..8).sumBy { (11-it) * cpf[it].toString().toInt() }.let { ((it + dv1 * 2) * 10).rem(11) }).let {
+        val dv2 = ((0..8).sumOf { (11-it) * cpf[it].toString().toInt() }.let { ((it + dv1 * 2) * 10).rem(11) }).let {
             if (it >= 10) 0 else it
         }
-
-        Log.i("AAAA", "Dvi: $dv1, dv2: $dv2")
-
-        if(cpf[9].toString().toInt() != dv1 || cpf[10].toString().toInt() != dv2) return "Insira um Cpf válido."
+        if(cpf[9].toString().toInt() != dv1 || cpf[10].toString().toInt() != dv2) return context.getString(
+                    R.string.validation_cpf_valid)
         return null
     }
 
@@ -41,12 +50,12 @@ object Validators {
 enum class Validator{
     EMAIL, NAME, PASSWORD, CPF;
 
-    fun validate(value: String) : String?{
+    fun validate(value: String?, context:Context) : String?{
         return when(this){
-            EMAIL -> Validators.emailValidator(value)
-            NAME -> Validators.nameValidator(value)
-            PASSWORD -> Validators.passwordValidator(value)
-            CPF -> Validators.cpfValidator(value)
+            EMAIL -> Validators.emailValidator(value, context)
+            NAME -> Validators.nameValidator(value, context)
+            PASSWORD -> Validators.passwordValidator(value, context)
+            CPF -> Validators.cpfValidator(value, context)
         }
     }
 }
