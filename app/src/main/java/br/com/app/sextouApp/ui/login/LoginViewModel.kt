fcb91e6ba2a0505fd883adfeca3f33ae3854ea0a
Rafael <rafafd@hotmail.com>
2021-07-10 13:36:14 -0300

    Add login with google and with email

--- app/src/main/java/br/com/app/sextouApp/ui/login/LoginViewModel.kt
+++ app/src/main/java/br/com/app/sextouApp/ui/login/LoginViewModel.kt
@@ -1,9 +1,16 @@
 package br.com.app.sextouApp.ui.login
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.app.sextouApp.utils.Validator
+import br.com.app.sextouApp.utils.Validators
 
 class LoginViewModel: ViewModel() {
     val login = MutableLiveData<String>()
     val password = MutableLiveData<String>()
+
+    fun formIsValid(context: Context): Boolean{
+        return Validator.EMAIL.validate(login.value, context) == null && password.value?.isNotBlank()?: false
+    }
 }
---
+++ app/src/main/java/br/com/app/sextouApp/utils/Constants.kt
@@ -0,0 +1,5 @@
+package br.com.app.sextouApp.utils
+
+const val SECURITY_PREFERENCES_NAME = "securityPreferences"
+const val USER_ID_PREFERENCE_KEY = "userId"
+const val RC_SIGN_IN_GOOGLE = 1
